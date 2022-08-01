9b5e3b492ea703bc8d986f97f60f1eddbd2785c8
Kevin Tang <kevin26428@gmail.com>
2022-08-01 15:20:35 -0500

    [MAIN-130] initial tictactoe getting started template that will be used in the tutorial (#15)
    * pcreate build-frontend action
    
    add build artifacts
    
    use node 16
    
    fix cra not building due to CI=true
    
    fix workflow to use correct auth method
    
    try a folder
    
    try just dot
    
    escape the dot
    
    back to dot
    
    build line from .gitignore
    
    frontend folder
    
    try going up a dir
    
    try to push build artifacts ourselves lmao
    
    testing this fucker
    
    wtf am i doing
    
    create branch if not existant
    
    add a git pull
    
    try to reset hard
    
    fix copy cmd
    
    trying something
    
    try rsync
    
    try something else
    
    tryign other things
    
    try another thing
    
    add commit messages
    
    fetch everything
    
    use main instead of getting started
    
    rename action to publish
    
    remove eslint and backend code
    
    add console log statements
    
    * add postinstall script
    
    * fix broken template

--- index.js
+++ index.js
@@ -1,53 +1,4 @@
 // TicTacToe Example
-const Status = Object.freeze({
-  PreGame: 'preGame',
-  InGame: 'inGame',
-  EndGame: 'endGame',
-});
-
-function getPlrMark(player, plrs) {
-  if (player.id === plrs[0].id) {
-    return 'X';
-  }
-  return 'O';
-}
-
-function getPlrFromMark(mark, plrs) {
-  return mark === 'X' ? plrs[0] : plrs[1];
-}
-
-function isWinningSequence(arr) {
-  return arr[0] != null && arr[0] === arr[1] && arr[1] === arr[2];
-}
-
-function isEndGame(board, plrs) {
-  // check if there is a winner
-  for (let i = 0; i < board.length; i += 1) {
-    const row = board[i];
-    const col = [board[0][i], board[1][i], board[2][i]];
-
-    if (isWinningSequence(row)) {
-      return [true, getPlrFromMark(row[0], plrs)];
-    } if (isWinningSequence(col)) {
-      return [true, getPlrFromMark(col[0], plrs)];
-    }
-  }
-
-  const d1 = [board[0][0], board[1][1], board[2][2]];
-  const d2 = [board[0][2], board[1][1], board[2][0]];
-  if (isWinningSequence(d1)) {
-    return [true, getPlrFromMark(d1[0], plrs)];
-  } if (isWinningSequence(d2)) {
-    return [true, getPlrFromMark(d2[0], plrs)];
-  }
-
-  // check for tie
-  if (board.some((row) => row.some((mark) => mark === null))) {
-    return [false, null];
-  }
-  return [true, null];
-}
-
 /**
  * Generic board game types
  * @type Player: json object, in the format of
@@ -80,17 +31,8 @@ function isEndGame(board, plrs) {
  * @returns {BoardGameResult}
  */
 function onRoomStart() {
-  return {
-    state: {
-      status: Status.PreGame,
-      board: [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null],
-      ],
-      winner: null, // null means tie if game is finished, otherwise set to the plr that won
-    },
-  };
+  console.log("onRoomStart")
+  return {};
 }
 
 /**
@@ -100,29 +42,8 @@ function onRoomStart() {
  * @returns {BoardGameResult}
  */
 function onPlayerJoin(player, boardGame) {
-  const { players, state } = boardGame;
-
-  // VALIDATIONS
-  // check if game has started
-  if (state.status !== Status.PreGame) {
-    throw new Error("game has already started, can't join the game!");
-  }
-
-  // TRANSFORMATIONS
-  // determine if we should start the game
-  if (players.length === 2) {
-    // start game
-    state.status = Status.InGame;
-    state.plrToMoveIndex = 0;
-    return {
-      state,
-      joinable: false,
-    };
-  }
-  return {
-    state,
-    joinable: true,
-  };
+  console.log("onPlayerJoin", player, boardGame)
+  return {};
 }
 
 /**
@@ -133,35 +54,8 @@ function onPlayerJoin(player, boardGame) {
  * @returns {BoardGameResult}
  */
 function onPlayerMove(player, move, boardGame) {
-  const { state, players } = boardGame;
-  const { board, plrToMoveIndex } = state;
-
-  // VALIDATIONS
-  // boardgame must be in the game
-  const { x, y } = move;
-  if (state.status !== Status.InGame) {
-    throw new Error("game is not in progress, can't make move!");
-  }
-  if (players[plrToMoveIndex].id !== player.id) {
-    throw new Error(`Its not this player's turn: ${player.username}`);
-  }
-  if (board[x][y] !== null) {
-    throw new Error(`Invalid move, someone already marked here: ${x},${y}`);
-  }
-
-  const plrMark = getPlrMark(player, players);
-  board[x][y] = plrMark;
-
-  // Check if game is over
-  const [isEnd, winner] = isEndGame(board, players);
-  if (isEnd) {
-    state.status = Status.EndGame;
-    state.winner = winner;
-    return { state, finished: true };
-  }
-
-  state.plrToMoveIndex = plrToMoveIndex === 0 ? 1 : 0;
-  return { state };
+  console.log("onPlayerJoin", player, move, boardGame)
+  return {};
 }
 
 /**
@@ -171,14 +65,8 @@ function onPlayerMove(player, move, boardGame) {
  * @returns {BoardGameResult}
  */
 function onPlayerQuit(player, boardGame) {
-  const { state, players } = boardGame;
-  state.status = Status.EndGame;
-  if (players.length === 1) {
-    const [winner] = players;
-    state.winner = winner;
-    return { state, joinable: false, finished: true };
-  }
-  return { joinable: false, finished: true };
+  console.log("onPlayerQuit", player, boardGame)
+  return {};
 }
 
 module.exports = {
